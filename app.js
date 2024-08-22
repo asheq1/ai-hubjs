aa132fda6b12fff8993a5fd1c8a88ff81d8d3f35
asheq1 <bsmrstu18fmb025@gmail.com>
2024-08-22 12:48:31 +0600

    card show

--- app.js
+++ app.js
@@ -22,15 +22,26 @@ const showAI = (techs) =>{
                 alt="" />
             </figure>
             <div class="card-body">
-                <h2 class="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+                <h2 class="card-title">Features</h2>
+                <div>
+                    <ul>
+                        <ol><span>1. </span>${tech?.features[0] || 'No data avaiable'}</ol>
+                        <ol><span>2. </span>${tech?.features[1] || 'No data avaiable'}</ol>
+                        <ol><span>3. </span>${tech?.features[2] || 'No data avaiable'}</ol>
+                    </ul>
+                </div>
+                <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
+                <h2 class="text-2xl font-bold">${tech.name}</h2>
+                <div class="flex">
+                    <img src="frame.png" alt="" />
+                    <p class='ml-2'>${tech.published_in}</p>
+                    <a href="" class="text-blue-600 visited:text-purple-600 ...">
+                        <img src="direction.png" alt="" />
+                    </a>
                 </div>
             </div>
         `;
         container.appendChild(div)
-
     })
 
 }
